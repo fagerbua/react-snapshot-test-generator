b0b95cdc7a0a8c42cec11f5781f7d934219213d9
Jarle Fagerheim <jarle.fagerheim@usit.uio.no>
2016-11-23 16:22:49 +0100

    support multiline props and string quotes

--- src/templates.js
+++ src/templates.js
@@ -7,7 +7,7 @@ export const jsxProp = (name, value) => `${name}={${value}}`
 export const jsxTag = (name, props) => props.length > 0 ? `<${name} ${props} />` : `<${name} />`
 
 export const testDescription = (componentName, componentProps) => `
-describe('${jsxTag(componentName, componentProps)}', () => {
+describe(\`${jsxTag(componentName, componentProps)}\`, () => {
   it('renders correctly', () => {
     snapshotTest(${jsxTag(componentName, componentProps)})
   })
